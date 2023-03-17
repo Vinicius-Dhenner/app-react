fa6a5538ec96da12d051b0f6fe423582f96254ff
Vinicius-Dhenner <Vinicius.dhenner2003@gmail.com>
2023-03-16 21:05:04 -0400

    second-commit

--- src/components/Users/index.tsx
+++ src/components/Users/index.tsx
@@ -1,7 +1,9 @@
+import { Button } from '../Button'
 import './style.css'
 
 type usersProps = {
     name : String
+    email : String
 }
 
 export function Users (props : usersProps) {
@@ -10,6 +12,8 @@ export function Users (props : usersProps) {
             <div className="users">
                 <section className="texts">
                     <h1>{props.name}</h1>
+                    <p>{props.email}</p>
+                    <Button text="Detalhes" type="primary"/>
                 </section>
             </div>
         </>
--- src/pages/Contacts/index.tsx
+++ src/pages/Contacts/index.tsx
@@ -6,13 +6,22 @@ import { users } from './services/api';
 
 const api = users;
 
+let position = 0;
+function positionApi () {
+    return position++;
+}
+
 export function Contacts () {
     return (
         <>
             <div className="contacts">
                 <Title text="Agenda de Contatos" />
                 <Search/>
-                <Users name={api[0].first}/>
+                <Users name={api[positionApi()].first + " " + api[positionApi()].last} email={api[positionApi()].email}/>
+                <Users name={api[positionApi()].first + " " + api[positionApi()].last} email={api[positionApi()].email}/>
+                <Users name={api[positionApi()].first + " " + api[positionApi()].last} email={api[positionApi()].email}/>
+                <Users name={api[positionApi()].first + " " + api[positionApi()].last} email={api[positionApi()].email}/>
+                <Users name={api[positionApi()].first + " " + api[positionApi()].last} email={api[positionApi()].email}/>
             </div>
         </>
     )
